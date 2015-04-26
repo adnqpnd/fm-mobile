1ae935f0ac421cc1f19fad804e9acdc0fb631a67
Adonique Pineda <devadonique@gmail.com>
2015-04-26 19:35:13 +0800

    fixed and add notif in login

--- www/js/controllers/login-controller.js
+++ www/js/controllers/login-controller.js
@@ -2,7 +2,17 @@
 
 angular.module('fMMobileApp')
 .controller('LoginCtrl',['$scope','$http','$state','authService','userService',function($scope,$http,$state,authService,userService){
-  $scope.error='';
+  $scope.error ='';
+  $scope.errorMessage = false;
+  
+  $scope.showErrorMessage = function (data,message) {
+    if(data === true){
+      $scope.error = message;
+    }
+
+    $scope.errorMessage = data;
+  };
+
   $scope.logIn = function (guestData) {
     authService.login('/mobile/login',guestData).success(function (data) {
         var status = data.status;
@@ -10,15 +20,14 @@ angular.module('fMMobileApp')
         console.log(data.status);
         if(status.code === 1) {
           authService.setToken(data.token);
-          // userService.getUser();
          userService.getUser().success(function (data) {
            $state.go('loadIn');  
-         });
-         
+         });   
         }else{
-          $scope.error = status.message;
+          // $scope.error = status.message;
 	      //$scope.showError = true;
-	      console.log($scope.error);
+        console.log(status.message);
+	      $scope.showErrorMessage(true,status.message);
         }
 
 	  }).error(function (error) {
