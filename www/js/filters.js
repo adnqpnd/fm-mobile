3909661fac5c11298c336e0ef850fbd4df721e1a
Adonique Pineda <devadonique@gmail.com>
2015-06-14 11:33:23 +0800

    delete fit;er

--- www/js/filters.js
+++ www/js/filters.js
@@ -1,22 +1,3 @@
 'use strict';
 
-angular.module("FMApp.filters",[])
-
-.filter("truckFilter", function ($filter) {
-    return function (data,id) {
-        console.log(data);
-        console.log(id);
-        if (angular.isArray(data)) {
-            var result = [];
-            angular.forEach(data, function(item){
-              if (item.truck_id.id === id){
-                 result.push(item);
-              }
-            });
-            console.log(result);
-            return result;
-        } else {
-            return data;
-        }
-    }
-});
+angular.module("FMApp.filters",[]);
