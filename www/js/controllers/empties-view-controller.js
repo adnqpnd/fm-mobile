b1dd9af04da0b3ad6fbd581cece99f312a64d0b6
Adonique Pineda <devadonique@gmail.com>
2015-06-17 01:02:57 +0800

    empties fixe

--- www/js/controllers/empties-view-controller.js
+++ www/js/controllers/empties-view-controller.js
@@ -2,16 +2,18 @@
 
 angular.module('FMApp.controllers')
 .controller('EmptiesViewCtrl',['$scope','$http','$state','authService','httpHost','_',
-  '$stateParams','$timeout','$filter','$cordovaDatePicker',function($scope,$http,$state,authService,httpHost,_,$stateParams,$timeout,$filter,$cordovaDatePicker){
+  '$stateParams','$timeout','$filter','$cordovaDatePicker',
+  function($scope,$http,$state,authService,httpHost,_,$stateParams,$timeout,$filter,$cordovaDatePicker){
   $scope.deliveries = [];
-  $scope.returns = [];
-  $scope.empties = {};
-  $scope.empties.deposit = 0;
+  $scope.returns = {};
+  $scope.returnsList = [];
+  $scope.totalDeposit = 0;
   var deliveryID = $stateParams.deliveryID;
   
   var getDeliverySelected = function () {
     $http.get(httpHost + "/delivery/list?id="+ deliveryID).success( function (data) {
-      $scope.deliveries = data;
+      $scope.deliveries = $scope.sortData(data[0].products,'sku_id.sku_name');
+      $scope.returns.sku = $scope.deliveries[0];
         console.log("Delivery Transaction Selected:");
         console.log($scope.deliveries);
         console.log(deliveryID);
@@ -23,23 +25,53 @@ angular.module('FMApp.controllers')
 
 
   getDeliverySelected();
-
+    $scope.combined = function (sku) {
+    return sku.sku_id.prod_id.brand_name+ ' ' + sku.sku_id.sku_name + ' ' + sku.sku_id.size;
+  }
   
-    $scope.submit = function (returns) {
-    console.log($scope.deliveries);
-    for(var i = 0 ; i < returns.length; i++){
-      console.log(i);
-      // console.log($scope.deliveries[i].products[0].sku_id.id);
-      console.log($scope.deliveries[i].products[0].sku_id.id);
-      returns[i].sku_id = $scope.deliveries[i].products[0].sku_id.id;
-      returns[i].sku_name = $scope.deliveries[i].products[0].sku_id.sku_name+" "+ $scope.deliveries[i].products[0].sku_id.size;
-      returns[i].bottlespercase = $scope.deliveries[i].products[0].sku_id.bottlespercase;
-    }
-    console.log(returns);
-    console.log($scope.empties.deposit);
+
+  $scope.addReturns = function (returns) {
     var returnsInfo = {
-      "returns":returns, 
-      "deposit": $scope.empties.deposit, 
+      "skuObject": returns.sku,
+      "sku_id": returns.sku.sku_id.id,
+      "sku_name": $scope.combined(returns.sku),
+      "bottlespercase": returns.sku.sku_id.bottlespercase,
+      "bottles": returns.bottles,
+      "cases": returns.cases,
+      "deposit": returns.deposit
+    };
+    $scope.returnsList.push(returnsInfo);
+    console.log($scope.returnsList);
+    var index = _.findIndex($scope.deliveries,{'id': returns.sku.id});
+    $scope.deliveries.splice(index,1);
+    $scope.totalDeposit += returnsInfo.deposit;
+    console.log($scope.totalDeposit);
+    
+    clearForm();
+
+  };
+
+  $scope.deleteReturns = function (index,returns) {
+     $scope.deliveries.push(returns.skuObject);
+     $scope.deliveries = $scope.sortData($scope.deliveries,'sku_id.sku_name');
+    $scope.returns.sku = $scope.deliveries[0];
+     $scope.returnsList.splice(index,1);
+     $scope.totalDeposit -= returns.deposit;
+    console.log($scope.totalDeposit);
+  }
+
+  var clearForm = function (){
+    $scope.deliveries = $scope.sortData($scope.deliveries,'sku_id.sku_name');
+    $scope.returns.sku = $scope.deliveries[0];
+    $scope.returns.bottles = null;
+    $scope.returns.cases = null;
+    $scope.returns.deposit = null;
+  }
+  
+    $scope.submit = function () {
+    var returnsInfo = {
+      "returns":$scope.returnsList, 
+      "deposit": $scope.totalDeposit, 
       "deliveryId": parseInt(deliveryID) 
     };
 
@@ -53,8 +85,6 @@ angular.module('FMApp.controllers')
       if(JWR.statusCode === 200){
         // $scope.bays.push(body);
           $state.go('app.empties');
-          $scope.returns = {};
-          $scope.empties.deposit = 0;
         $scope.$digest();
       }
   }); 
